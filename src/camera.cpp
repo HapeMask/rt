add3eb81a6b074c4dbd4b7a166e30d1fdcd48b2e
Gabe Schwartz <hapemask@gmail.com>
2010-03-14 06:07:29 +0000

    Added camera impl.
    --HG--
    extra : convert_revision : svn%3Ac32f086a-3a94-11de-9146-fdf1833cc731/trunk%40243

---
+++ src/camera.cpp
@@ -0,0 +1,11 @@
+#include "camera.hpp"
+#include "ray.hpp"
+#include <cmath>
+
+void camera::getRay(const int& x, const int& y, ray& r){
+	float upFrac = (float)(halfH - y) / (float)halfH;
+	float sideFrac = (float)(halfW - x) / (float)halfW;
+	vec3 v = (left * sideFrac) + (up * upFrac);
+	r.direction() = normalize(v);
+	r.origin() = position;
+}
