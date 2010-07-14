1b8f3a86fda4d711eb3d3d7de7cb8ed17d0bb290
Gabe Schwartz <hapemask@gmail.com>
2010-07-14 20:52:10 +0000

    Added ESC to pause (only pause, not resume).
    --HG--
    extra : convert_revision : svn%3Ac32f086a-3a94-11de-9146-fdf1833cc731/trunk%40389

--- src/main.cpp
+++ src/main.cpp
@@ -124,6 +124,13 @@ int main(int argc, char* args[]){
                                 return 0;
                             }
                             break;
+                        case SDLK_ESCAPE:
+                            if(!paused){
+                                cerr << "Pausing..." << endl;
+                                paused = true;
+                            }
+
+                            break;
                         case '-':
                             linTmScale -= 0.25f;
                             f.setLinearTonemapScale(linTmScale);
