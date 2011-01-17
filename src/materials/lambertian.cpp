a01f4d1ad2c65d89dc55b80a8a4f4ed4fbc6d461
Gabe Schwartz <hapemask@gmail.com>
2011-01-17 09:05:39 +0000

    Added missing fabsf.
    --HG--
    extra : convert_revision : svn%3Ac32f086a-3a94-11de-9146-fdf1833cc731/trunk%40500

--- src/materials/lambertian.cpp
+++ src/materials/lambertian.cpp
@@ -27,7 +27,7 @@ inline const rgbColor lambertianBrdf::f(const vec3& wo, const vec3& wi) const {
 }
 
 inline const float lambertianBrdf::pdf(const vec3& wo, const vec3& wi) const {
-    return bsdf::cosTheta(wi) * INVPI;
+    return fabsf(bsdf::cosTheta(wi) * INVPI);
 }
 
 inline void lambertianBrdf::updateFromUVTexture(const vec2& uv) {
