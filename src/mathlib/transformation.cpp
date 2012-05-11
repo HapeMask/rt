4eda1b7bffb2202d273571cfba920ab352fef344
Gabe Schwartz <hapemask@gmail.com>
2012-05-11 18:05:49 -0400

    Removed some redundant return type qualifiers, moved stuff inline.

--- src/mathlib/transformation.cpp
+++ src/mathlib/transformation.cpp
@@ -7,51 +7,7 @@
 
 #include <cmath>
 
-const point3 transform3d::apply(const point3& p) const {
-	const vec4 v = mat * vec4(p.x, p.y, p.z, 1.f);
-	return point3(v.x/v.w, v.y/v.w, v.z/v.w);
-}
-
-const vec3 transform3d::apply(const vec3& v) const {
-	return vec3(
-			mat(0,0) * v.x + mat(0,1) * v.y + mat(0,2) * v.z,
-			mat(1,0) * v.x + mat(1,1) * v.y + mat(1,2) * v.z,
-			mat(2,0) * v.x + mat(2,1) * v.y + mat(2,2) * v.z
-		);
-}
-
-const ray transform3d::apply(const ray& r) const {
-	return ray(apply(r.origin), apply(r.direction));
-}
-
-const point3 transform3d::unapply(const point3& p) const {
-	const vec4 v = inv * vec4(p.x, p.y, p.z, 1.f);
-	return point3(v.x/v.w, v.y/v.w, v.z/v.w);
-}
-
-const vec3 transform3d::unapply(const vec3& v) const {
-	return vec3(
-			inv(0,0) * v.x + inv(0,1) * v.y + inv(0,2) * v.z,
-			inv(1,0) * v.x + inv(1,1) * v.y + inv(1,2) * v.z,
-			inv(2,0) * v.x + inv(2,1) * v.y + inv(2,2) * v.z
-		);
-}
-
-const ray transform3d::unapply(const ray& r) const {
-    return ray(unapply(r.origin), unapply(r.direction));
-}
-
-const transform3d transform3d::operator*(const transform3d& t) const {
-	return transform3d(*this) *= t;
-}
-
-transform3d& transform3d::operator*=(const transform3d& t){
-	mat *= t.mat;
-	inv = mat.inverse();
-	return (*this);
-}
-
-const transform3d translate(const float& dx, const float& dy, const float& dz){
+transform3d translate(const float& dx, const float& dy, const float& dz){
 	mat4 m;
 	m(0,3) = dx;
 	m(1,3) = dy;
@@ -65,7 +21,7 @@ const transform3d translate(const float& dx, const float& dy, const float& dz){
 	return transform3d(m, i);
 }
 
-const transform3d scale(const float& sx, const float& sy, const float& sz){
+transform3d scale(const float& sx, const float& sy, const float& sz){
 	mat4 m;
 	m(0,0) = sx;
 	m(1,1) = sy;
@@ -79,7 +35,7 @@ const transform3d scale(const float& sx, const float& sy, const float& sz){
 	return transform3d(m, i);
 }
 
-const transform3d lookAt(const point3& pos, const point3& look, const vec3& up){
+transform3d lookAt(const point3& pos, const point3& look, const vec3& up){
 	const vec3 dir(normalize(look - pos));
 	const vec3 right(cross(dir, normalize(up)));
 	const vec3 newUp(cross(right, dir));
@@ -94,7 +50,7 @@ const transform3d lookAt(const point3& pos, const point3& look, const vec3& up){
     return transform3d(mat4(v).inverse());
 }
 
-const transform3d perspective(const float& fov, const float& near, const float& far){
+transform3d perspective(const float& fov, const float& near, const float& far){
 	const float invTan = 1.f / tan(radians(fov) / 2.f);
 	const float v[4][4] = {
 		{1, 0, 0, 0},
--- src/mathlib/transformation.hpp
+++ src/mathlib/transformation.hpp
@@ -6,43 +6,63 @@
 #include "ray.hpp"
 
 class transform3d{
-	public:
-		transform3d(){}
+    public:
+        transform3d() = default;
+        transform3d(const mat4& m, const mat4& i) : mat(m), inv(i) {}
+        transform3d(const mat4& m) : mat(m), inv(m.inverse()) {}
 
-		transform3d(const mat4& m, const mat4& i){
-			mat = m;
-			inv = i;
-		}
+        inline const mat4& matrix() const{ return mat; }
+        inline const mat4& inverse() const{ return inv; }
 
-		transform3d(const mat4& m){
-			mat = m;
-			inv = m.inverse();
-		}
+        inline point3 apply(const point3& p) const {
+            const vec4 v = mat * vec4(p, 1.f);
+            return point3(v.x/v.w, v.y/v.w, v.z/v.w);
+        }
 
-		const mat4& matrix() const{
-			return mat;
-		}
+        inline vec3 apply(const vec3& v) const {
+            return vec3(
+                    mat(0,0) * v.x + mat(0,1) * v.y + mat(0,2) * v.z,
+                    mat(1,0) * v.x + mat(1,1) * v.y + mat(1,2) * v.z,
+                    mat(2,0) * v.x + mat(2,1) * v.y + mat(2,2) * v.z
+                    );
+        }
 
-		const mat4& inverse() const{
-			return inv;
-		}
+        inline ray apply(const ray& r) const {
+            return ray(apply(r.origin), apply(r.direction));
+        }
 
-		const point3 apply(const point3& p) const;
-		const vec3 apply(const vec3& v) const;
-		const ray apply(const ray& r) const;
+        inline point3 unapply(const point3& p) const {
+            const vec4 v = inv * vec4(p.x, p.y, p.z, 1.f);
+            return point3(v.x/v.w, v.y/v.w, v.z/v.w);
+        }
 
-		const point3 unapply(const point3& p) const;
-		const vec3 unapply(const vec3& v) const;
-		const ray unapply(const ray& r) const;
+        inline vec3 unapply(const vec3& v) const {
+            return vec3(
+                    inv(0,0) * v.x + inv(0,1) * v.y + inv(0,2) * v.z,
+                    inv(1,0) * v.x + inv(1,1) * v.y + inv(1,2) * v.z,
+                    inv(2,0) * v.x + inv(2,1) * v.y + inv(2,2) * v.z
+                    );
+        }
 
-		const transform3d operator*(const transform3d& t) const;
-		transform3d& operator*=(const transform3d& t);
+        inline ray unapply(const ray& r) const {
+            return ray(unapply(r.origin), unapply(r.direction));
+        }
 
-	private:
-		mat4 mat, inv;
+        inline transform3d operator*(const transform3d& t) const {
+            return transform3d(*this) *= t;
+        }
+
+        inline transform3d& operator*=(const transform3d& t) {
+            mat *= t.mat;
+            inv = mat.inverse();
+            return (*this);
+        }
+
+    private:
+        mat4 mat, inv;
 };
 
-const transform3d translate(const float& dx, const float& dy, const float& dz);
-const transform3d scale(const float& sx, const float& sy, const float& sz);
-const transform3d lookAt(const point3& pos, const point3& look, const vec3& up);
-const transform3d perspective(const float& fov, const float& near, const float& far);
+transform3d translate(const float& dx, const float& dy, const float& dz);
+transform3d scale(const float& sx, const float& sy, const float& sz);
+transform3d lookAt(const point3& pos, const point3& look, const vec3& up);
+transform3d perspective(const float& fov, const float& near, const float& far);
